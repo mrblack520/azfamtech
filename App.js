535ebd4c43cf67f04ac45bd73faccef50a6c3550
HassanDurra <109581409+HassanDurra@users.noreply.github.com>
2023-02-06 18:01:05 +0500

    Add files via upload
    Hogeya

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View,SafeAreaView } from 'react-native'
+import Login from './Allpages/login.js'
+export default class App extends Component {
+  render() {
+    return (
+// safe view area
+
+     <Login></Login>
+
+)
+  }
+}
